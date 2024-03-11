6e01fa13c7daa58dfcf0f96aac316dfde263c4a6
Ficker Yook <149360871+FickerYook@users.noreply.github.com>
2024-03-11 12:53:41 +0700

    'ttttt'

--- src/app/app.firebase.config.ts
+++ src/app/app.firebase.config.ts
@@ -5,4 +5,12 @@ export const FIREBASE_CONFIG = {
     storageBucket: "authenapp-409df.appspot.com",
     messagingSenderId: "813329815329",
     appId: "1:813329815329:web:7e753c2c9b775ab7ec5060"
+    // apiKey: "AIzaSyApH1W6lnG4Nl-AsWedV2c9vshhvK92buA",
+    // authDomain: "project2-d1312.firebaseapp.com",
+    // projectId: "project2-d1312",
+    // storageBucket: "project2-d1312.appspot.com",
+    // messagingSenderId: "195163362675",
+    // appId: "1:195163362675:web:cc2329bbe80fe37f512106"
+
+
 };
